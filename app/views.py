11c02fd31c90fd79ec3d0c53c10331823191b4c2
Danielwang405 <danielwang405@gmail.com>
2018-01-21 05:29:37 -0800

    added lat and long

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base, Student, Event
 from app import app
+import requests
+import json
 
 engine = create_engine('sqlite:///events.db')
 Base.metadata.bind = engine
@@ -37,8 +39,18 @@ def host():
         start = request.form['timeStartIn']
         end = request.form['timeEndIn']
         desc = request.form['descIn']
-        event = Event(name=n,loc_name=loc,subject=subj,date=date,start=start,end=end,desc=desc)
+        #event = Event(name=n,loc_name=loc,subject=subj,date=date,start=start,end=end,desc=desc)
 
+        #using loc get coordinates
+        #first separate loc with + to obtain address
+        #https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+        address = loc.replace(" ","+")
+        result =  json.loads(requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='+address+"&key="+"AIzaSyAPIKI9rNnmIyT-5frqg6aSqaAS3hUA69k").content)
+        #print(result["results"][0].geometry.location.lat)
+        lat = result["results"][0]["geometry"]["location"]["lat"]
+        lng = result["results"][0]["geometry"]["location"]["lng"]
+        event = Event(name=n,loc_name=loc,loc_lat=lat,loc_long=lng,subject=subj,date=date,start=start,end=end,desc=desc)
+            
         session.add(event)
         session.commit()
         return redirect(url_for('home'))
