e589f29bb599ed2cf269384b9cec747dd22569f0
andrewni <ajni@ucdavis.edu>
2018-01-21 01:54:26 -0800

    html to template

--- app/views.py
+++ app/views.py
@@ -11,7 +11,18 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
+def home():
+    return render_template("main.html")
+
 @app.route('/index')
 def index():
     all_students = session.query(Student).all()
-    return render_template("index.html",students = all_students)
+    return render_template("main.html",students = all_students)
+
+@app.route('/main')
+def main():
+    return render_template("main.html")
+
+@app.route('/host')
+def host():
+    return render_template("host.html")
